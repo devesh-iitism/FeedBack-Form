19c54a632d29bf6f968741f469044c32b2c748c2
unknown <dcdev001@gmail.com>
2021-05-09 12:09:35 +0530

    serialize user

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,10 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
 passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
@@ -12,15 +16,14 @@ passport.use(
         callbackURL: '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-        User.findOne({googleId: profile.id})
-            .then((existingUser) => {
-                if(existingUser) {
-                    done(null, existingUser);
-                } else {
-                    new User({googleId: profile.id})
-                        .save()
-                        .then(user => done(null, user));
-                }
-            })
+        User.findOne({googleId: profile.id}).then((existingUser) => {
+            if(existingUser) {
+                done(null, existingUser);
+            } else {
+                new User({googleId: profile.id})
+                    .save()
+                    .then(user => done(null, user));
+            }
+        })
     })
 );

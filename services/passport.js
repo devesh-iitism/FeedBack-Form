0db65e877ac7993254e6498f00b8201a186e950c
unknown <dcdev001@gmail.com>
2021-05-09 11:38:59 +0530

    add user in database only if the user does not exists

--- services/passport.js
+++ services/passport.js
@@ -12,6 +12,13 @@ passport.use(
         callbackURL: '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-        new User({googleId: profile.id}).save();
+        User.findOne({googleId: profile.id})
+            .then((existingUser) => {
+                if(existingUser) {
+
+                } else {
+                    new User({googleId: profile.id}).save();
+                }
+            })
     })
 );

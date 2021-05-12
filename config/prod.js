93b9e23c9ca67b7a22858f4bcda65ff81050200b
unknown <dcdev001@gmail.com>
2021-05-12 11:55:22 +0530

    minor change

--- config/prod.js
+++ config/prod.js
@@ -1,14 +1,6 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOFLE_CLIENT_SECRET,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 };
-
-/*
-googleClientID: '1057637748416-1urg88g3219idjb6hvrm50vofh13e4fp.apps.googleusercontent.com',
-    mongoURI: 'mongodb+srv://newuser:Chaturvedi@123@cluster0.wtmuf.mongodb.net/emaily?retryWrites=true&w=majority',
-    googleClientSecret: '_eu1bXQpgYsJP_BLdBgYh1ON',
-    cookieKey: 'asjvhakjhfvjkhfjkhehfkefkjefk'
-
-*/

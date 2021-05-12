b7215d5f44790085e6a8ac2a08d995da09d7d461
unknown <dcdev001@gmail.com>
2021-05-12 10:06:29 +0530

    set up production environment

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '499196365589-o9rc9hghiq2saf7pso31thhih420aibt.apps.googleusercontent.com',
+    mongoURI: 'mongodb+srv://devesh_1011:Chaturvedi@123@cluster0.gem31.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    googleClientSecret: 'wXsFaRW9xnVG7ukPRgrQNyV_',
+    cookieKey: 'asjvhakjhfvjkhfjkhehfkefkjefk'
+};

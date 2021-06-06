b6d64637f7dca3188e3162e5f013e30081eeff6e
unknown <dcdev001@gmail.com>
2021-06-07 03:49:38 +0530

    setup react-router and materialize-css

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import App from './components/App';
+import reducers from './reducers';
+import 'materialize-css/dist/css/materialize.min.css';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>, 
